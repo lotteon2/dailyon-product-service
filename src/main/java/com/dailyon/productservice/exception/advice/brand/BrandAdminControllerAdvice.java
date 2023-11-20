d169e8ae5781ee251fee3428db1b2bfb81cb38a0
wakkpu <ho0524870@gmail.com>
2023-11-20 19:46:10 +0900

    [FEAT] update brand exception - not exists

--- src/main/java/com/dailyon/productservice/exception/advice/brand/BrandAdminControllerAdvice.java
+++ src/main/java/com/dailyon/productservice/exception/advice/brand/BrandAdminControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.dailyon.productservice.exception.advice.brand;
 
+import com.dailyon.productservice.exception.BrandNotFoundException;
 import com.dailyon.productservice.exception.DuplicatedBrandException;
 import com.dailyon.productservice.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,15 @@ public class BrandAdminControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse duplicatedBrandException(DuplicatedBrandException e) {
         return ErrorResponse.builder()
-                .message(e.getMessage())
+                .message("이미 존재하는 브랜드 이름입니다")
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse brandNotFoundException(BrandNotFoundException e) {
+        return ErrorResponse.builder()
+                .message("존재하지 않는 브랜드입니다")
                 .build();
     }
 }
