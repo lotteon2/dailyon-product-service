115c2bfd9b62ce33421d9e7387fa13d9f8e52308
wakkpu <ho0524870@gmail.com>
2023-11-20 14:53:26 +0900

    [FEAT] brand create request / response

---
+++ src/main/java/com/dailyon/productservice/dto/response/CreateBrandResponse.java
@@ -0,0 +1,10 @@
+package com.dailyon.productservice.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CreateBrandResponse {
+    private Long brandId;
+}
