7ed125e84e803038f41c896ffc355a85789eef4f
wakkpu <ho0524870@gmail.com>
2023-11-22 11:20:29 +0900

    [FIX] Batch Size for children

--- src/main/java/com/dailyon/productservice/entity/Category.java
+++ src/main/java/com/dailyon/productservice/entity/Category.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ public class Category extends BaseEntity {
     @JoinColumn(name = "master_category_id")
     private Category masterCategory;
 
-//    @BatchSize(size = 10) // https://velog.io/@joonghyun/SpringBoot-JPA-JPA-Batch-Size%EC%97%90-%EB%8C%80%ED%95%9C-%EA%B3%A0%EC%B0%B0
+    @BatchSize(size = 10) // https://velog.io/@joonghyun/SpringBoot-JPA-JPA-Batch-Size%EC%97%90-%EB%8C%80%ED%95%9C-%EA%B3%A0%EC%B0%B0
     @OneToMany(mappedBy = "masterCategory", fetch = FetchType.LAZY)
     @Builder.Default
     private List<Category> childrenCategories = new ArrayList<>();
