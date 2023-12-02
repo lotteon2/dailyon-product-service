983c2f575f83243e8713d70552dffb97c66763f1
전종민 <ho05248@naver.com>
2023-12-03 02:12:14 +0900

    [FEAT] 상품 목록 조회 수정 controller

--- src/main/java/com/dailyon/productservice/product/controller/ProductController.java
+++ src/main/java/com/dailyon/productservice/product/controller/ProductController.java
@@ -24,12 +24,11 @@ public class ProductController {
     }
 
     @GetMapping
-    ResponseEntity<ReadProductSliceResponse> readProductSlice(@RequestParam(required = false) Long brandId,
+    ResponseEntity<ReadProductSliceResponse> readProductSlice(@RequestParam Long lastId,
+                                                              @RequestParam(required = false) Long brandId,
                                                               @RequestParam(required = false) Long categoryId,
                                                               @RequestParam(required = false) Gender gender,
-                                                              @RequestParam ProductType type,
-                                                              @RequestParam(required = false) String query,
-                                                              @PageableDefault(page = 0, size = 8) Pageable pageable) {
-        return ResponseEntity.status(HttpStatus.OK).body(productService.readProductSlice(brandId, categoryId, gender, type, query, pageable));
+                                                              @RequestParam ProductType type) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.readProductSlice(lastId, brandId, categoryId, gender, type));
     }
 }
