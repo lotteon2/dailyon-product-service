64ae5005f6a706b9f83a3c6f52be5756e878ef20
전종민 <ho05248@naver.com>
2023-12-04 00:54:41 +0900

    [FEAT] OOTD에서 상품 검색 controller

---
+++ src/main/java/com/dailyon/productservice/product/dto/response/ReadOOTDSearchSliceResponse.java
@@ -0,0 +1,25 @@
+package com.dailyon.productservice.product.dto.response;
+
+import com.dailyon.productservice.product.entity.Product;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class ReadOOTDSearchSliceResponse {
+    private boolean hasNext;
+    private List<ReadOOTDSearchResponse> products;
+
+    public static ReadOOTDSearchSliceResponse fromEntity(Slice<Product> products) {
+        return ReadOOTDSearchSliceResponse.builder()
+                .hasNext(products.hasNext())
+                .products(products.stream()
+                        .map(ReadOOTDSearchResponse::fromEntity)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
