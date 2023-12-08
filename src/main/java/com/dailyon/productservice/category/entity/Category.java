195586857edcb1a7a358e365cd29ade403c433f2
wakkpu <ho0524870@gmail.com>
2023-12-08 15:15:03 +0900

    [FIX] 삭제 시 이름 쓰레기값으로 변경

--- src/main/java/com/dailyon/productservice/category/entity/Category.java
+++ src/main/java/com/dailyon/productservice/category/entity/Category.java
@@ -82,5 +82,6 @@ public class Category extends BaseEntity {
 
     public void softDelete() {
         this.deleted = true;
+        this.name = this.name + "_" + System.currentTimeMillis();
     }
 }
