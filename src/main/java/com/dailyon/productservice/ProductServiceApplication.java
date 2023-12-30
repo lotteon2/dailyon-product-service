2f6ab896ae23a380cc085374a1452ae021cc89d6
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-30 17:45:48 +0900

    [FEAT] add seoul timezone

--- src/main/java/com/dailyon/productservice/ProductServiceApplication.java
+++ src/main/java/com/dailyon/productservice/ProductServiceApplication.java
@@ -6,14 +6,22 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableJpaAuditing
 @SpringBootApplication
 public class ProductServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ProductServiceApplication.class, args);
+  }
 
+  @PostConstruct
+  public void setTimezoneToSeoul() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+  }
 }
