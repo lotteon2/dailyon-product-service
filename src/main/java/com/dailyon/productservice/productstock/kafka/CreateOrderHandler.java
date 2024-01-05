050f4fccf17897cc707caa4ac7076f340b5c24da
wakkpu <ho0524870@gmail.com>
2024-01-05 11:47:39 +0900

    [FIX] kafka error handling

--- src/main/java/com/dailyon/productservice/productstock/kafka/CreateOrderHandler.java
+++ src/main/java/com/dailyon/productservice/productstock/kafka/CreateOrderHandler.java
@@ -1,16 +1,16 @@
 package com.dailyon.productservice.productstock.kafka;
 
-import com.dailyon.productservice.common.exception.InsufficientQuantityException;
 import com.dailyon.productservice.productstock.kafka.dto.OrderDto;
 import com.dailyon.productservice.productstock.service.ProductStockService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+import dailyon.domain.common.KafkaTopic;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CreateOrderHandler {
@@ -19,18 +19,17 @@ public class CreateOrderHandler {
     private final CreateOrderProductHandler createOrderProductHandler;
     private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = "create-order")
+    @KafkaListener(topics = KafkaTopic.CREATE_ORDER)
     public void consume(String message, Acknowledgment ack) {
         OrderDto orderDto = null;
         try {
             orderDto = objectMapper.readValue(message, OrderDto.class);
             productStockService.deductProductStocks(orderDto.getProductInfos());
             createOrderProductHandler.produce(orderDto);
-        } catch (InsufficientQuantityException e) {
+        } catch (Exception e) {
             assert orderDto != null;
+            log.error(e.getMessage());
             cancelOrderHandler.produce(orderDto);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
         } finally {
             ack.acknowledge();
         }
