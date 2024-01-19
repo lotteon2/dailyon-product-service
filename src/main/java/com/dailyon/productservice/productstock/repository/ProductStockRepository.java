1fd5accc5b84d403b19f752e9af29e3a791cc48a
wakkpu <ho0524870@gmail.com>
2024-01-19 12:39:31 +0900

    [FIX] removed

--- src/main/java/com/dailyon/productservice/productstock/repository/ProductStockRepository.java
+++ src/main/java/com/dailyon/productservice/productstock/repository/ProductStockRepository.java
@@ -4,19 +4,15 @@ import com.dailyon.productservice.product.entity.Product;
 import com.dailyon.productservice.productstock.entity.ProductStock;
 import com.dailyon.productservice.productstock.entity.ProductStockId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
 public interface ProductStockRepository extends JpaRepository<ProductStock, ProductStockId>, ProductStockCustomRepository {
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<ProductStock> findProductStocksByProductOrderByProductSize(Product product);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
